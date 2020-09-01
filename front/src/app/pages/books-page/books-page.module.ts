20111761df8fe5dc7b6451cb82e1bf67fdc4fb1a
piotrrus <irache@wp.pl>
2020-09-01 21:55:02 +0200

    Update books-page.module.ts

--- front/src/app/pages/books-page/books-page.module.ts
+++ front/src/app/pages/books-page/books-page.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/modules/shared.module';
 import { RouterModule } from '@angular/router';
 import { BooksPageComponent } from './books-page.component';
-import { BooksTableComponent } from './components/books-table/books-table.component';
 import { BooksService } from 'src/app/shared/services';
 
 @NgModule({
-  declarations: [BooksPageComponent, BooksTableComponent],
+  declarations: [BooksPageComponent],
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: BooksPageComponent },
-      // { path: '/:id', component: BooksPageComponent },
+      { path: '', component: BooksPageComponent }
     ])
   ],
   providers: [BooksService],
