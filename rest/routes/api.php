ed1fae9b429e890f8023b027fb32bb25667bb248
piotrrus <irache@wp.pl>
2020-09-02 22:17:41 +0200

    Update api.php

--- rest/routes/api.php
+++ rest/routes/api.php
@@ -20,17 +20,19 @@ use Illuminate\Http\Request;
 
 Route::get('authors', 'Api\AuthorsController@index');
 Route::get('authors/show/{id}', 'Api\AuthorsController@show');
-Route::post('authors/store', 'Api\AuthorsController@store');
+Route::post('authors/create', 'Api\AuthorsController@create');
 Route::post('authors/update/{id}', 'Api\AuthorsController@update');
 
 Route::get('books', 'Api\BooksController@index');
 Route::get('books/show/{id}', 'Api\BooksController@show');
+Route::get('books/update/{id}', 'Api\BooksController@update');
+Route::get('books/create', 'Api\BooksController@create');
 Route::post('books/title/{title}', 'Api\BooksController@title');
 Route::post('books/author/{author}', 'Api\BooksController@author');
 
 Route::get('genres', 'Api\GenresController@index');
 Route::post('genres/update/{id}', 'Api\GenresController@update');
-Route::post('genres/store', 'Api\GenresController@update');
+Route::post('genres/create', 'Api\GenresController@create');
 Route::get('publishers', 'Api\PublishersController@index');
 Route::post('publishers/update/{id}', 'Api\PublishersController@update');
 Route::post('publishers/store', 'Api\PublishersController@store');
